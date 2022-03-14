5575afd6dbf3bacd18161f3d5ef7ae7b81ed187f
sugimomoto <sugimomoto@gmail.com>
2022-03-15 08:03:45 +0900

    first commit

---
+++ src/main/java/sugimomoto/App.java
@@ -0,0 +1,20 @@
+package sugimomoto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        User user = new User();
+
+        user.setName("hello");
+        System.out.println(user.getName());
+
+        
+    }
+}
---
+++ src/main/java/sugimomoto/User.java
@@ -0,0 +1,8 @@
+package sugimomoto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+}
